6a02882024c17b6438395fdc508880df6a153f64
pambohermane-source <pambohermane@gmail.com>
2026-04-21 09:21:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ const cookieParser = require('cookie-parser');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(require('cookie-parser')());
+app.use(cookieParser());
 
 require('./models/database');
 
 const authRoutes = require('./routes/auth');
 const dashboardRoutes = require('./routes/dashboard');
 const reclamationsRoutes = require('./routes/reclamations');
+const statistiquesRoutes = require('./routes/statistiques');
 
 app.use('/', authRoutes);
 app.use('/dashboard', dashboardRoutes);
 app.use('/reclamation', reclamationsRoutes);
-app.use('/fichier', reclamationsRoutes);
+app.use('/statistiques', statistiquesRoutes);
 
 app.get('/', (req, res) => res.redirect('/dashboard'));
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log('BCEG Reclamations demarre sur le port ' + PORT);
-  console.log('Acces : http://localhost:' + PORT + '/login');
 });
