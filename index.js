7fb02e335ae79513a946038a20fa9053442793dc
pambohermane-source <pambohermane@gmail.com>
2026-04-21 14:05:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const path = require('path');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,12 +16,35 @@ const reclamationsRoutes = require('./routes/reclamations');
 const statistiquesRoutes = require('./routes/statistiques');
 const enquetesRoutes = require('./routes/enquetes');
 
+// ROUTES PUBLIQUES - sans connexion obligatoire
+const upload = multer({ dest: path.join(__dirname, 'uploads/') });
+
+app.get('/depot-reclamation', (req, res) => {
+  // Servir la page publique directement
+  req.url = '/public';
+  reclamationsRoutes(req, res, function(){});
+});
+
+app.post('/depot-reclamation/soumettre', upload.single('fichier'), (req, res) => {
+  req.url = '/public/soumettre';
+  reclamationsRoutes(req, res, function(){});
+});
+
+// ROUTES ENQUETE CLIENT - sans connexion
+app.get('/enquete-client/:numero', (req, res, next) => {
+  req.url = '/client/' + req.params.numero;
+  enquetesRoutes(req, res, next);
+});
+app.post('/enquetes/soumettre/:numero', (req, res, next) => {
+  enquetesRoutes(req, res, next);
+});
+
+// ROUTES PROTEGEES
 app.use('/', authRoutes);
 app.use('/dashboard', dashboardRoutes);
 app.use('/reclamation', reclamationsRoutes);
 app.use('/statistiques', statistiquesRoutes);
 app.use('/enquetes', enquetesRoutes);
-app.use('/enquete-client', enquetesRoutes);
 
 app.get('/', (req, res) => res.redirect('/dashboard'));
 
